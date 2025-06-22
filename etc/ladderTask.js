1ea9fde2898bd18a007c48c4945e056881d7fe51
Triddov <akinin.stepan7@gmail.com>
2025-06-22 20:33:24 +0300

    chore: rebra(i)nding

--- etc/ladderTask.js
+++
@@ -1,8 +0,0 @@
-const ladderTask = {
-    step: 0,
-    up(){ this.step++; return this},
-    down() { this.step--; return this },
-    show() { console.log(`now step is ${this.step}`); return this }
-}
-
-ladderTask.up().up().up().down().show()
